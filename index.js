d2aa7a536836392162dea561879a8cd733b81426
jeung <ran22314@naver.com>
2023-11-23 18:50:35 +0900

    https

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 8080;
-app.use(express.json())
+const path = require('path');
 
-// app.get('/', (req, res) => {
-// res.json({
-// success: true,
-// });
-// });
+const keyPath = path.join('./key.pem');
+const certPath = path.join('./cert.pem');
 
 const analyze = require('./routes/flask.js');
-app.use("/analyze", analyze)
 
 const test10 = require('./routes/dbConnection.js');
-app.use("/dbConnection", test10)
 
-// const acquireData = require('./routes/acquireData.js');
-// app.use("/acquireData", acquireData)
+https.createServer(
+    {key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath)},
 
-app.listen(port, () => {
-console.log(`server is listening at localhost:${port}`);
-});
+    app.use(express.json()),
+
+    // app.get('/', (req, res) => {
+    // res.json({
+    // success: true,
+    // });
+    // });
+    
+    
+    app.use("/analyze", analyze),
+    
+    
+    app.use("/dbConnection", test10),
+    
+    // const acquireData = require('./routes/acquireData.js');
+    // app.use("/acquireData", acquireData)
+    
+    app.use('/', (req, res) => {
+        res.send('connect');
+        console.log(`server is listening at localhost:${port}`);
+    })
+).listen(port)
--- routes/flask.js
+++ routes/flask.js
@@ -1,11 +1,23 @@
 const express = require('express');
+const https = require('https');
 // var request = require('request');
 const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 var db = require('../lib/db.js');
 const return_query = require('../lib/acquireData.js');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+const keyPath = path.join('./key.pem');
+const certPath = path.join('./cert.pem');
+
+const httpsAgent = new https.Agent({
+    rejectUnauthorized: false, // 자체 서명된 인증서를 사용할 경우 필요
+    cert: fs.readFileSync(certPath),
+    key: fs.readFileSync(keyPath)
+  });
+
+router.post('/', async(req, res) => {
     try{
         let body = req.body;
         const sensor_name = body.sensor;
@@ -15,20 +27,36 @@ router.post('/', (req, res) => {
         const end = body.end;
     // const vibe_data = body.file_name;
 
-        const flaskServerUrl = 'http://165.246.44.131:5000/analyze';
+        const flaskServerUrl = 'https://127.0.0.1:5000/analyze';
         sql = return_query.return_query(infra, sensor_name,start,end);
-    // db 접근
-        db.query(sql, async(err, result, fields) => {
-            if (err) throw err;
-            console.log(result);
-            const response = await axios.post(flaskServerUrl, { data: result, transform }, {
-                params: { sensor: sensor_name }
-                });
-            res.json(response.data);
+        const dbResult = await new Promise((resolve, reject) => {
+            db.query(sql, (err, result, fields) => {
+                if (err) reject(err);
+                resolve(result);
+            });
         });
+
+        console.log(dbResult);
+        const response = await axios.post(flaskServerUrl, { data: dbResult, transform }, {
+            params: { sensor: sensor_name },
+            httpsAgent
+        });
+        res.json(response.data);
     } catch (error) {
+        console.error(error);
         res.status(500).send('Error sending data to Flask server: ' + error);
     }
+    // db 접근
+    //     db.query(sql, (err, result, fields) => {
+    //         if (err) throw err;
+    //         console.log(result);
+    //         const response = axios.post(flaskServerUrl, { data: result, transform }, {
+    //             params: { sensor: sensor_name }
+    //             }, httpsAgent).then(response => console.log(response.data), res.json(response.data)).catch(error => console.error(error));
+    //     });
+    // } catch (error) {
+    //     res.status(500).send('Error sending data to Flask server: ' + error);
+    // }
     // let body = req.body;
     // const sensor_name = body.sensor || 'sensor_one';
     // // const vibe_data = body.file_name;
